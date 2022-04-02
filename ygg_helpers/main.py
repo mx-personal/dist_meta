27d59464550ef3061fc6ac280468a56b691936ff
mx-personal <maximefer@hotmail.fr>
2022-04-02 23:27:07 +0100

    carrying ygg changes

--- ygg_helpers/main.py
+++ ygg_helpers/main.py
@@ -1,7 +1,8 @@
-import importlib_metadata as imp_md
-import yaml
 import sys
 from os.path import exists
+from collections import namedtuple
+import importlib_metadata as imp_md
+import yaml
 
 
 def get_internal_info(distribution: str) -> {}:
@@ -10,7 +11,7 @@ def get_internal_info(distribution: str) -> {}:
     entry_points = [{'name': ep.name, 'path': r'{0}\Scripts\{1}'.format(sys.prefix, ep.name)} for ep in eps_console]
     pkgs = [pkg for pkg, dists in imp_md.packages_distributions().items() if distribution in dists] # packages names
     libraries = [{'name': pkg, 'path': str(dist.locate_file(pkg))} for pkg in pkgs]
-    requirements = [r'{0}\requirements.txt'.format(lib['path']) for lib in libraries if exists(r'{0}\requirements.txt'.format(lib['path']))]
+    requirements = [{"requirements":r'{0}\requirements.txt'.format(lib['path'])} for lib in libraries if exists(r'{0}\requirements.txt'.format(lib['path']))]
 
     return {
         "environment": {'name': sys.prefix.split("\\")[-1], 'path': sys.prefix},
@@ -29,3 +30,35 @@ def dump_internal_info_venv():
     distribution = sys.argv[1]
     path_dump = r'{0}\ygginfo-{1}.yaml'.format(sys.prefix, distribution)
     dump_internal_info(distribution, path_dump)
+
+
+class DistInfo(object):
+    PathInfo = namedtuple("PathInfo", ["name", "path"])
+
+    def __init__(self, configs):
+        self._configs = configs
+
+    @classmethod
+    def from_yaml(cls, path):
+        with open(path) as file:
+            try:
+                configs = yaml.safe_load(file)
+            except yaml.YAMLError as exc:
+                print(exc)
+        return DistInfo(configs)
+
+    @property
+    def entry_points(self):
+        return [self.__class__.PathInfo(name=ep['name'], path=ep['path']) for ep in self._configs['entry_points']]
+
+    @property
+    def environment(self):
+        return self.__class__.PathInfo(name=self._configs['environment']['name'], path=self._configs['environment']['path'])
+
+    @property
+    def libraries(self):
+        return [self.__class__.PathInfo(name=lib['name'], path=lib['path']) for lib in self._configs['libraries']]
+
+    @property
+    def requirements(self):
+        return [self.__class__.PathInfo(name=lib['name'], path=lib['path']) for lib in self._configs['requirements']]
